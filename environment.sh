baf453bee47a5e7c9fd42794eb11bce49a5179cd
root <chenyuming052@gmail.com>
2025-05-09 17:40:50 +0930

    cotr

--- environment.sh
+++ environment.sh
@@ -1,13 +1,19 @@
-# # 1. 把 conda 函数加载到当前 shell
-# source /opt/conda/etc/profile.d/conda.sh
+# source /yuming/CoTr/environment.sh
+# nnUNet_plan_and_preprocess -t 71 --verify_dataset_integrity
+# python run_training.py -gpu='0' -taks='' -outpath=''
+# python run_training.py -gpu='0' -outpath='' -val --val_folder=''
 
-# # 2. 激活 cotr 环境
-# conda activate cotr
+# 1. 把 conda 函数加载到当前 shell
+source /opt/conda/etc/profile.d/conda.sh
+
+# 2. 激活 cotr 环境
+conda activate cotr
 
 pip install --upgrade pip setuptools wheel
 pip install simpleitk==2.0.2
 pip install matplotlib
 pip install batchgenerators==0.21
+pip install ipykernel
 
 cd /yuming/CoTr/nnUNet
 
